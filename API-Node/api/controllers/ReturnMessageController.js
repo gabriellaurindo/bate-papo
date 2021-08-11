20dbbb0692492c8acaee3ae5f374d2a8e7f5d7e3
Gabriel Laurindo <gabrielvitorlaurindo@gmail.com>
2021-08-10 22:04:22 -0300

    Pequenas correções

--- API-Node/api/controllers/ReturnMessageController.js
+++ API-Node/api/controllers/ReturnMessageController.js
@@ -4,12 +4,11 @@ const Message = require("../models/MessageModel")
 //Função para retornar mensagens com base no sender e receiver
 function ReturnMessage (req,res){
     const {sender, receiver} = req.body
-        Message.find({$or:[{sender, receiver}, {sender:receiver, receiver:sender}]}).sort({date: 'asc'}).then(messages=>{
+        Message.find({$or:[{sender, receiver}, {sender:receiver, receiver:sender}]}).sort({date: 'desc'}).then(messages=>{
             res.statusCode = 201
             res.send({messages})
         })
- 
 }
 
-//Exportando Módulo
+//Exportando Controller
 module.exports = ReturnMessage
--- API-Node/api/controllers/SendMessageController.js
+++ API-Node/api/controllers/SendMessageController.js
@@ -1,20 +1,18 @@
-//Importando Módulos
+//Importando Models
 const User = require("../models/UserModel")
 const Message = require("../models/MessageModel")
 
 //Função de login
 function SendMessage (req,res){
     const {conteudo, sender, receiver} = req.body
-    var dataAtual = new Date().toJSON().slice(0,10).replace(/-/g,'/');
+    var dataAtual = new Date();
     Message.create({conteudo, sender, receiver}).catch(err =>{
-        console.log("Houve um erro: " + err)
         res.statusCode = 403
         res.send({Error:err})
     })
-    console.log("Deu certo")
     res.statusCode = 201
     res.send({Mess:"Sucess"})
 }
 
-//Exportando Módulo
+//Exportando Controller
 module.exports = SendMessage
